661b593c30600851cdd63a15a10a91a55e407ae7
MrSaan <langlangbolang87@gmail.com>
2023-01-12 19:57:52 +0700

    edit title layout

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,11 @@ import { Box, Link } from '@chakra-ui/react'
 const Footer = () => {
   return (
     <Box align="center" opacity={0.4} fontSize="sm">
-      &copy; {new Date().getFullYear()} <Link href="https://www.craftz.dog/" target="_blank">Created : Takuya Matsuyama</Link>. All Rights Reserved.
+      &copy; {new Date().getFullYear()}{' '}
+      <Link href="https://www.craftz.dog/" target="_blank">
+        By : Takuya
+      </Link>
+      . All Rights Reserved.
     </Box>
   )
 }
