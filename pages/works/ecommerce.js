1bd7963722d666344c0dda4cd6e883d02c5f24ab
MrSaan <langlangbolang87@gmail.com>
2023-01-12 20:14:53 +0700

    add images and edit articles

--- pages/works/ecommerce.js
+++ pages/works/ecommerce.js
@@ -1,51 +1,48 @@
-import {
-    Container,
-    Badge,
-    Link,
-    List,
-    ListItem,
-} from '@chakra-ui/react'
+import { Container, Badge, Link, List, ListItem } from '@chakra-ui/react'
 import { Title, WorkImage, Meta } from '../../components/work'
 import P from '../../components/paragraph'
 import Layout from '../../components/layouts/article'
 
 const Work = () => (
-    <Layout title="Inkdrop">
-        <Container>
-            <Title>
-                E-Commerce <Badge>Portofolio Projek-</Badge>
-            </Title>
-            <P>
-                Aplikasi toko baju online.
-                <Link href="https://drive.google.com/file/d/1vonVRSV8lUtESLZ0EYTwWppuXhHDGnaz/view?usp=sharing">
-                    lebih lanjut
-                </Link>
-            </P>
-            <List ml={4} my={4}>
-                <ListItem>
-                    <Meta>Platform</Meta>
-                    <span>Website</span>
-                </ListItem>
-                <ListItem>
-                    <Meta>Stack</Meta>
-                    <span>Laravel, Livewire, TailwindCss</span>
-                </ListItem>
-                <ListItem>
-                    <Meta>Github</Meta>
-                    <Link href="https://github.com/MrSaan/e-commerce">
-                        Aplikasi belanja online
-                    </Link>
-                </ListItem>
-            </List>
+  <Layout title="Inkdrop">
+    <Container>
+      <Title>
+        E-Commerce <Badge>Portofolio Projek-</Badge>
+      </Title>
+      <P>
+        Aplikasi toko baju online. <br />
+        terintegrasi dengan Midtrans sebagai alat pembayaran. terintegrasi
+        dengan Mailchimp untuk manajemen email. responsive terhadap mobile
+        device / tablet
+        <Link href="https://drive.google.com/file/d/1vonVRSV8lUtESLZ0EYTwWppuXhHDGnaz/view?usp=sharing">
+          lebih lanjut
+        </Link>
+      </P>
+      <List ml={4} my={4}>
+        <ListItem>
+          <Meta>Platform</Meta>
+          <span>Website</span>
+        </ListItem>
+        <ListItem>
+          <Meta>Stack</Meta>
+          <span>Laravel, Livewire, TailwindCss, Docker</span>
+        </ListItem>
+        <ListItem>
+          <Meta>Github</Meta>
+          <Link href="https://github.com/MrSaan/e-commerce">
+            Aplikasi belanja online
+          </Link>
+        </ListItem>
+      </List>
 
-            <WorkImage src="/images/works/ecommerce_1.png" alt="ecommerce" />
-            <WorkImage src="/images/works/ecom_6.png" alt="ecommerce" />
-            <WorkImage src="/images/works/ecom_2.png" alt="ecommerce" />
-            <WorkImage src="/images/works/ecom_3.png" alt="ecommerce" />
-            <WorkImage src="/images/works/ecom_4.png" alt="ecommerce" />
-            <WorkImage src="/images/works/ecom_5.png" alt="ecommerce" />
-        </Container>
-    </Layout>
+      <WorkImage src="/images/works/ecommerce_1.png" alt="ecommerce" />
+      <WorkImage src="/images/works/ecom_6.png" alt="ecommerce" />
+      <WorkImage src="/images/works/ecom_2.png" alt="ecommerce" />
+      <WorkImage src="/images/works/ecom_3.png" alt="ecommerce" />
+      <WorkImage src="/images/works/ecom_4.png" alt="ecommerce" />
+      <WorkImage src="/images/works/ecom_5.png" alt="ecommerce" />
+    </Container>
+  </Layout>
 )
 
 export default Work
--- pages/works/ishari.js
+++ pages/works/ishari.js
@@ -1,45 +1,38 @@
-import {
-    Container,
-    Badge,
-    Link,
-    List,
-    ListItem,
-} from '@chakra-ui/react'
+import { Container, Badge, Link, List, ListItem } from '@chakra-ui/react'
 import { Title, WorkImage, Meta } from '../../components/work'
 import P from '../../components/paragraph'
 import Layout from '../../components/layouts/article'
 
 const Work = () => (
-    <Layout title="Inkdrop">
-        <Container>
-            <Title>
-                Kitab ISHARI NU <Badge>Portofolio Projek-</Badge>
-            </Title>
-            <P>
-                Aplikasi mendengarkan dan membaca shalawat ISHARI NU.
-            </P>
-            <List ml={4} my={4}>
-                <ListItem>
-                    <Meta>Platform</Meta>
-                    <span>Website, Progressive Web Apps (PWA)</span>
-                </ListItem>
-                <ListItem>
-                    <Meta>Stack</Meta>
-                    <span>Laravel, Inertia, Vite, TailwindCss</span>
-                </ListItem>
-                <ListItem>
-                    <Meta>Github</Meta>
-                    <Link href="https://github.com/MrSaan/e-commerce">
-                        Source Github
-                    </Link>
-                </ListItem>
-            </List>
+  <Layout title="Inkdrop">
+    <Container>
+      <Title>
+        Kitab ISHARI NU <Badge>Portofolio Projek-</Badge>
+      </Title>
+      <P>Aplikasi mendengarkan dan membaca shalawat ISHARI NU.</P>
+      <List ml={4} my={4}>
+        <ListItem>
+          <Meta>Platform</Meta>
+          <span>Website, Progressive Web Apps (PWA)</span>
+        </ListItem>
+        <ListItem>
+          <Meta>Stack</Meta>
+          <span>Laravel, Inertia, Vite, TailwindCss</span>
+        </ListItem>
+        <ListItem>
+          <Meta>Github</Meta>
+          <Link href="https://github.com/MrSaan/e-commerce">Source Github</Link>
+        </ListItem>
+      </List>
 
-            <WorkImage src="/images/works/ishari_1.png" alt="ecommerce" />
-            <WorkImage src="/images/works/ishari_2.png" alt="ecommerce" />
-            <WorkImage src="/images/works/ishari_3.png" alt="ecommerce" />
-        </Container>
-    </Layout>
+      <WorkImage src="/images/works/homepage.png" alt="ecommerce" />
+      <WorkImage src="/images/works/muhud.png" alt="ecommerce" />
+      <WorkImage src="/images/works/pimpinan.png" alt="ecommerce" />
+      <WorkImage src="/images/works/lagu.png" alt="ecommerce" />
+      <WorkImage src="/images/works/love.png" alt="ecommerce" />
+      <WorkImage src="/images/works/muhud mobile.png" alt="ecommerce" />
+    </Container>
+  </Layout>
 )
 
 export default Work
